b54dd68214674f06ed07c212ce877d3407c714d5
WubzyGD <wubzygd@gmail.com>
2021-12-10 18:53:35 -0700

    fix ctx pos when window is scrollable

--- scripts/contextmenu/createcontext.js
+++ scripts/contextmenu/createcontext.js
@@ -3,5 +3,5 @@ module.exports = (e, target, window) => {
     let ctx = document.getElementById('ctx');
     ctx.style.display = 'block';
     ctx.style.left = `${Math.min(e.pageX, (window.innerWidth - (ctx.clientWidth + 2)))}px`;
-    ctx.style.top = `${Math.min(e.pageY, (window.innerHeight - (ctx.clientHeight + 2)))}px`;
+    ctx.style.top = `${Math.min(e.pageY, ((window.innerHeight + window.scrollY) - (ctx.clientHeight + 2)))}px`;
 };
