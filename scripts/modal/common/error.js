d04d4a2c1748a3967f37cbcfc05fa5964ac4ff6e
WubzyGD <wubzygd@gmail.com>
2021-12-10 18:15:42 -0700

    begin dynamic error modal

---
+++ scripts/modal/common/error.js
@@ -0,0 +1,18 @@
+const preModal = require("../pre");
+const postModal = require('../post');
+
+module.exports = (name, text, after = () => {}) => {
+    preModal('error-modal');
+    let modal = document.createElement('div');
+    modal.className = 'modal';
+    modal.id = 'error-modal';
+    document.body.appendChild(modal);
+    let title = document.createElement('h2');
+    title.innerHTML = `Error - ${name}`;
+    modal.appendChild(title);
+    let err = document.createElement('p');
+    err.innerHTML = text;
+    modal.appendChild(err);
+    after('error-modal');
+    postModal();
+};
